380c5e9bc8ca67ca55b244ce3a6f8f1d1e68fecf
Will G <william.gilroy15@gmail.com>
2018-01-09 23:02:12 +0000

    09 jan notes

--- Program/Blackjack/Counting_AI.py
+++ Program/Blackjack/Counting_AI.py
@@ -204,7 +204,7 @@ class Testing_Class:
                 print(key, chances[key])
 
 if __name__ == "__main__":
-    range_of_values = range(1, 12)
+    range_of_values = [1,2,3,4,5,6,7,8,9,10,11]
     number_of_suits = 4
     CI = Counting_AI(range_of_values, number_of_suits)
     test = Testing_Class()
