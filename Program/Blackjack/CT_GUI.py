c10dae716bb021aeb27e7eeefde3fa24ee834ded
Will Gilroy <william.gilroy15@gmail.com>
2018-03-23 19:57:04 +0000

    added realtional window

--- Program/Blackjack/CT_GUI.py
+++ Program/Blackjack/CT_GUI.py
@@ -53,14 +53,14 @@ class Init_Win(Window):
         self.hide()
         if win_to_open == "iso_comp":
             self.isolated_comp = Iso_Win(ct=self.ct, root=self, parent=tk.Toplevel())
-            #self.isolated_comp.run()
         elif win_to_open == "rel_comp":
-            pass  # implement this!!
+            self.rel_comp = Rel_Win(ct=self.ct, root=self, parent=tk.Toplevel())
 
 # isolated user comparison
 # enter user name and get data about them, in isolation
+# todo turn this and the other window
 class Iso_Win(Window):
-    def __init__(self, ct, root, parent=None, geometry="400x400"):
+    def __init__(self, ct, root, parent, geometry="400x400"):
         super().__init__(parent, geometry)
         self.ID = "iso_comp"
         self.ct = ct
@@ -91,13 +91,58 @@ class Iso_Win(Window):
         if not valid_id:
             self.res_label.config(text="User not Found")
             return False
+        self.res_label.config(text="User Found")
         self.ct.output_player_wr(id)
 
     def clear_default(self, *args):
-        print(args)
         if self.default_text:
             self.default_text = False
             self.un_entry.delete(0, "end")
+
+    def back(self):
+        self.root.show()
+        self.destroy()
+
+class Rel_Win(Window):
+    def __init__(self, ct, root, parent, geometry="400x400"):
+        super().__init__(parent, geometry)
+        self.ID = "rel_comp"
+        self.ct = ct
+        self.root = root
+
+        self.default_text_1 = True
+        self.default_text_2 = True
+
+    def build_widgets(self, fr):
+        self.title = tk.Label(fr, text="Relational Comparison")
+        self.title.grid(row=0, column=0)
+
+        self.un1_entry = tk.Entry(fr)
+        self.un1_entry.insert(0, "Enter Username 1 here")
+        self.un1_entry.bind("<Button-1>", lambda *args: self.clear_default(1))
+        self.un1_entry.grid(row=1, column=0)
+
+        self.un2_entry = tk.Entry(fr)
+        self.un2_entry.insert(0, "Enter Username 2 here")
+        self.un2_entry.bind("<Button-1>", lambda *args: self.clear_default(2))
+        self.un2_entry.grid(row=2, column=0)
+
+        self.realtions_btn = tk.Button(fr, text="Relations") # implement the command for this
+        self.realtions_btn.grid(row=3, column=0)
+
+        self.back_btn = tk.Button(fr, text="Back", command=self.back)
+        self.back_btn.grid(row=4, column=0)
+
+        self.res_label = tk.Label(fr, text="")
+        self.res_label.grid(row=5, column=0)
+
+    def clear_default(self, entry_id, *args):
+        if entry_id == 1 and self.default_text_1:
+            self.default_text_1 = False
+            self.un1_entry.delete(0, "end")
+        elif entry_id == 2 and self.default_text_2:
+            self.default_text_2 = False
+            self.un2_entry.delete(0, "end")
 
     def back(self):
         self.root.show()
