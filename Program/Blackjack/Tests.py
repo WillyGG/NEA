98e91e0a3f52c39b50cb68ed610df9fa3cd152cf
WillyGG <William.Gilroy15@gmail.com>
2018-04-18 12:34:06 +0100

    debugged first test

--- Program/Blackjack/Tests.py
+++ Program/Blackjack/Tests.py
@@ -1,21 +1,25 @@
-from DB.DB_Wrapper import DB_Wrapper
+import sys, os
+sys.path.append(os.path.realpath("./DB"))
+from DB_Wrapper import DB_Wrapper
 
-class Testing:
+class T:
     def __init__(self):
         self.db_wrapper = DB_Wrapper("DB/Blackjack.sqlite")
 
     def test_1aii(self):
         results = []
-        for game_id in range(1000,1020):
+        for game_id in range(1000,2000):
             query = """
                     SELECT * FROM Moves
-                    WHERE game_id = {0} AND hand_val_before=17
+                    WHERE game_id = {0} AND hand_val_before=17 AND player_id="nn"
                     """.format(game_id)
-            result = self.db_wrapper.execute_queries(query)
-            results.append(result)
+            result = self.db_wrapper.execute_queries(query, get_result=True)
+            if result != []:
+                results.append([game_id] + result)
         for result in results:
             print(result)
             print()
 
-t = Testing()
-t.test_1aii()
+if __name__ == "__main__":
+    t = T()
+    t.test_1aii()
